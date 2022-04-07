d0544a653b98461d07bd0d5767df703e94e9ac85
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-04-07 10:07:27 +0100

    Debugging jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,10 @@
 import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
   verbose: true,
-  automock: true
-}
-
+  automock: false,
+};
 
 export default config;
--- src/__tests__/hello.test.ts
+++ src/__tests__/hello.test.ts
@@ -1,9 +1,21 @@
 import * as Hello from "../hello";
 
-const { randomDice } = jest.requireActual<typeof Hello>("../hello");
+// Jest automatically mocks imports, so to get access to actual functions
+// we have to do the following - must be an easier way
+// NOTE: Yes, there is an easier way - turn OFF automock in jest.config.ts
+
+// const { randomDice, say } = jest.requireActual<typeof Hello>("../hello");
+
+//
+
+const lessThan7 = (n: Number) => n < 7;
 
 describe("Dice", () => {
-    test("Not even wrong!", () => {
-        expect(randomDice()).toBe(7);
-    });
+  test("Not even wrong!", () => {
+    expect(Hello.randomDice()).toBeLessThan(7);
+  });
+
+  test("Check Age", () => {
+    expect(Hello.Age).toBe(23);
+  });
 });
